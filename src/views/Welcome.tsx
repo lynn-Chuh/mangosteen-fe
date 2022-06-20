106d0cf3bbca8f579bb83e1f1b23f3b8d340331f
lynchuh <632955934@qq.com>
2022-06-20 14:46:38 +0800

    动画优化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,13 +1,33 @@
-import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
+import { NavigationGuardNext, RouteLocationNormalized, RouteLocationNormalizedLoaded, RouteRecordRaw, RouterView } from "vue-router";
 import s from './Welcome.module.scss'
-import { ref, Transition, VNode } from "vue";
+import { defineComponent, ref, Transition, VNode, watchEffect } from "vue";
 
 
-const routeActionRef = ref(0) // 0 未设置，1前进 -1后退 
-export default {
+const routeActionRef = ref({
+  enter: s.slide_fade_enter_from,
+  leave: s.slide_fade_leave_to
+})
+
+const Welcome = {
   name: 'Welcome',
-  render(){
-    return <div class={[s.wrapper, 'flex-col','items-center']}>
+  beforeRouteUpdate(to:RouteLocationNormalized,from:RouteLocationNormalized,next:NavigationGuardNext){
+    const _t = parseFloat(to.path.slice(-1))
+    const _f = parseFloat(from.path.slice(-1))
+    if(!isNaN(_t)&& !isNaN(_f)){
+      _t<_f ?// 后退
+        routeActionRef.value = {
+          leave: s.slide_fade_enter_from,
+          enter: s.slide_fade_leave_to
+        } :
+        routeActionRef.value = {
+          enter: s.slide_fade_enter_from,
+          leave: s.slide_fade_leave_to
+        }
+    }
+    next()
+  },
+  setup(){
+    return ()=> <div class={[s.wrapper, 'flex-col','items-center']}>
       <header class='flex-col items-center'>
         <svg-icon class={s.icon} icon="mangosteen"/>
         <h1>山竹记账</h1>
@@ -16,9 +36,9 @@ export default {
         <RouterView name="main">
           {
             ({Component:C,route:R}:{Component: VNode,route: RouteLocationNormalizedLoaded }) =>
-            <Transition 
-              enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
-              leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}
+            <Transition  
+              enterFromClass={routeActionRef.value.enter} enterActiveClass={s.slide_fade_enter_active}
+              leaveToClass={routeActionRef.value.leave} leaveActiveClass={s.slide_fade_leave_active}
             >
               {C}
             </Transition>
@@ -29,3 +49,6 @@ export default {
     </div>
   }
 }
+// console.log(Welcome)
+
+export default Welcome
