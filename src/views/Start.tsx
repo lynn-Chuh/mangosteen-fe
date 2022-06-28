43a07a5cc6de9f43814033bec7d9aad7939261c9
lynchuh <632955934@qq.com>
2022-06-28 14:39:07 +0800

    开始页基本样式完成

--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -2,12 +2,20 @@ import { defineComponent } from "vue";
 import Button from "../components/Button";
 import Center from "../components/Center";
 import FloatButton from "../components/FloatButton";
+import NavBar from "../components/NavBar";
 import s from './Start.module.scss'
 export default defineComponent({
   name: 'Start',
   setup(){
     return ()=> <div >
-      <nav>menu</nav>
+      <nav>
+        <NavBar>
+          {{
+            icon: ()=> <svg-icon icon="menu" />,
+            default: '山竹记账'
+          }}
+        </NavBar>
+      </nav>
       <Center class={s.icon_wrapper}>
         <svg-icon icon="pig" class={s.icon}/>
       </Center>
