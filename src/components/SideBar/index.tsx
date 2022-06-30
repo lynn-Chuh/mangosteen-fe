4e888e9213c10e92de42472b180af66d8351a5a3
lynchuh <632955934@qq.com>
2022-06-30 15:44:22 +0800

    完成sidebar组件

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,48 @@
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import Overlay from '../Overlay';
+import s from './index.module.scss'
+export default defineComponent({
+  name: 'SideBar',
+  emits:['close','update:show'],
+  props:{
+    show: {
+      type:Boolean,
+      default: false
+    }
+  },
+  setup(props,context){
+    const handleUpdateShow =(value:boolean)=>{
+      context.emit('close',value)
+      context.emit('update:show',value)
+    }
+
+    const actions = [
+      {name:'统计图标',icon:'chart',to:''},
+      {name:'导出数据',icon:'export',to:''},
+      {name:'记账提醒',icon:'notify',to:''},
+    ]
+
+    return ()=> <Overlay show={props.show} onUpdate:show={handleUpdateShow} position="left" width="70vw">
+      <div class={s.wrapper}>
+        <section class={s.user}>
+          <h2>未登录用户</h2>
+          <p>点击这里登陆</p>
+        </section>
+        <nav>
+          <ul class={s.action_list}>
+            { ...actions.map((_)=><li key={_.name}>
+                <RouterLink to={_.to} class={s.action}  >
+                  <svg-icon icon={_.icon} class={s.icon}/>
+                  <span class={s.text}>{_.name}</span>
+                </RouterLink>
+
+              </li>
+              )
+            }
+          </ul>
+        </nav>
+      </div>
+    </Overlay>
+  }
+})
--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -1,28 +1,36 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Button from "../components/Button";
 import Center from "../components/Center";
 import FloatButton from "../components/FloatButton";
 import NavBar from "../components/NavBar";
-import s from './Start.module.scss'
+import SideBar from "../components/SideBar";
+import s from "./Start.module.scss";
 export default defineComponent({
-  name: 'Start',
-  setup(){
-    return ()=> <div >
-      <nav>
-        <NavBar>
-          {{
-            icon: ()=> <svg-icon icon="menu" />,
-            default: '山竹记账'
-          }}
-        </NavBar>
-      </nav>
-      <Center class={s.icon_wrapper}>
-        <svg-icon icon="pig" class={s.icon}/>
-      </Center>
-      <div class={s.button_wrapper}>
-        <Button class={s.button}>开始记账</Button>
+  name: "Start",
+  setup() {
+    const OverlayVisible = ref(false);
+    const onClickMenu = () => {
+      OverlayVisible.value = true;
+    };
+    return () => (
+      <div>
+        <nav>
+          <NavBar>
+            {{
+              icon: () => <svg-icon icon="menu" onClick={onClickMenu} />,
+              default: () => "山竹记账",
+            }}
+          </NavBar>
+        </nav>
+        <Center class={s.icon_wrapper}>
+          <svg-icon icon="pig" class={s.icon} />
+        </Center>
+        <div class={s.button_wrapper}>
+          <Button class={s.button}>开始记账</Button>
+        </div>
+        <FloatButton icon="add" draggable/>
+        <SideBar v-model:show={OverlayVisible.value}/>
       </div>
-      <FloatButton icon="add" />
-    </div>
-  }
-})
+    );
+  },
+});
