b6920378490eb72b266d00fb46cba9dad089661d
lynchuh <632955934@qq.com>
2022-08-10 16:40:34 +0800

    chore 添加根目录 alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import {createSvg} from './vite_plugins/svg'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
   plugins: [
     vue(),
     vueJsx({
       transformOn: true,
       mergeProps: true
     }),
-    createSvg('./src/icons/svg/')
-  ],
-  // base:''
+    createSvg('./src/icons/svg/'),
+  ]
 })
